cbf9fb8729b0f80d4c85eaaf6d75ce669d8d7659
Flavio Montoril <flavio.montorilll@gmail.com>
2024-08-13 12:02:25 -0300

    criadas paginas index, lampada,maincss e mainjs

---
+++ main.js
@@ -0,0 +1,5 @@
+document.writeln("Este texto veio de um arquivo externo");
+document.writeln("O arquivo se chama externo.js");
+document.writeln("Ele pode ser lido atravez do comando SRC");
+document.writeln("Para ler um arquivo externo usa-se a seguinte sintaxe:\n");
+document.writeln("Utilize: src=&#34meuscript.js&#34 ");
